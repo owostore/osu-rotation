65e59bace8336327dfbb6e2e26388b05050c4dd1
nighthawk <73394830+owostore@users.noreply.github.com>
2022-11-17 17:59:46 +0900

    Create jsonFileProcessor.lua

---
+++ lib/jsonFileProcessor.lua
@@ -0,0 +1,32 @@
+--[[
+    Date : 2016.01.16 
+    Creator : Elkiss
+    Email : jominhyuk87@gmail.com
+]]--
+ 
+-- file read
+function FileRead(filePath)
+    local data = nil
+    local handle = io.open(filePath, "r")
+ 
+    if handle then
+        data = json:decode(handle:read("*a"))
+        io.close(handle)
+    end
+    
+    return data
+end
+ 
+-- file write
+function FileWrite(filePath, data, pretty)    
+    local handle = io.open(filePath, "w+")
+    
+    if handle then
+        if pretty then
+            handle:write(json:encode_pretty(data))
+        else
+            handle:write(json:encode(data))
+        end
+        io.close(handle)
+    end
+end
